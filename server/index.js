bfae9624003d3f76aa54168cc205c787a01e9b9c
comradekaushik <anandkaushik260@gmail.com>
2025-08-14 11:21:46 +0530

    @comradekaushik initialized index.js and express server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,21 @@
-//Start @comradekaushik
+//Start @comradekaushik 14AUG2025 
+const express = require("express");
+const {jwt} = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    const myobject = {
+        working : "yes",
+        myarray : [`Response time ${Date.now()}`]
+
+
+    }
+    const myjson = JSON.stringify(myobject)
+    res.send(myjson);
+})
+app.listen(8081, ()=>{
+    console.log(`Server Started ${Date.now()}`);
+
+})
 
