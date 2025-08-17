8f34301f5b4f4d7ccb7900d485d229d90840a3e5
shaan <adityakm5500@gmail.com>
2025-08-17 22:34:40 +0530

    mail has been added.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 //Start @comradekaushik 14AUG2025 
 const express = require("express");
 const {jwt} = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+        user: "adityakm5500@gmail.com",
+        pass: "dkdl idlo cpcb lfzq"
+    }
+})
 
 app.get("/", (req,res)=>{
     const myobject = {
@@ -16,16 +25,35 @@ app.get("/", (req,res)=>{
     const myjson = JSON.stringify(myobject)
     res.send(myjson);
 })
-app.post("/signup", (req,res)=>{
+app.post("/signup", async(req,res)=>{
     const body = req.body;
-    console.log(body);
-    const responsesignup = {
-        signup : "sucessful"
+    console.log("signup body:",body);
+    // const responsesignup = {
+    //     signup : "sucessful"
+    // }
+    // res.send(JSON.stringify(responsesignup));
+
+
+    try{
+        await transporter.sendMail({
+            from: '"fileflash app" <adityakm5500@gmail.com>',
+            to: body.email,
+            subject: "Signup Successful 🎉",
+            text: `Welcome ${body.name || "User" }! Your signup was successfull.`,
+            html:`
+                <h3>Welcome ${body.name || "User"} 🎉<h3>
+                <p>We're excited to have you onboard. Your signup was succesfull!</p>
+                <p><b>Date:</b> ${new Date().toLocaleString()}</p>
+            `
+        });
+        res.json({signup:"successful", emailSent:true});
+
+    } catch(err){
+        console.error("Email error: ", err);
+        res.status(500).json({ signup: "failed", error:"Email not sent"});
     }
-    res.send(JSON.stringify(responsesignup));
+});
 
-
-})
 app.post("/login", (req,res)=>{
 
 })
